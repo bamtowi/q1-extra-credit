ce7bec17a1b4213af69650d8345976bcc3ed326f
bamtowi <91894409+bamtowi@users.noreply.github.com>
2023-11-10 11:08:19 +0800

    Update Artist.java

--- src/gitrep/Artist.java
+++ src/gitrep/Artist.java
@@ -1,15 +1,47 @@
 package gitrep;
 
 public class Artist {
-    String name;
-    String genre;
-    int yrs;
-    double rate;
+    private String name;
+    private String genre;
+    private int yrs;
+    private double rate;
     
     public Artist(String n, String g, int y, double r){
-        name = n;
-        genre = g;
-        yrs = y;
-        rate = r;
+        this.name = n;
+        this.genre = g;
+        this.yrs = y;
+        this.rate = r;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYrs() {
+        return yrs;
+    }
+
+    public void setYrs(int yrs) {
+        this.yrs = yrs;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
     }
 }
