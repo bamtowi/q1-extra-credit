79b4ec2800cdf0858c230fe15059bd10da81a05b
bamtowi <91894409+bamtowi@users.noreply.github.com>
2023-11-10 11:05:39 +0800

    Update GitRep.java

--- src/gitrep/GitRep.java
+++ src/gitrep/GitRep.java
@@ -3,39 +3,34 @@ package gitrep;
 public class GitRep {
 
     public static void main(String[] args) {
-        // Objects
-        String artist1 = "Radiohead";
-        String genre1 = "Alt Rock";
-        int yrs1 = 38;
-        double rate1 = 4.28;
-        System.out.println("Artist 1\n Name: " + artist1 + "\n Genre: " + genre1 + "\n Years active: " + yrs1 + "\n Highest Album Rating: " + rate1 + "\n~~~~~~~~~~~~~~~~~~~~");
+        // Custom class
+        Artist artist1 = new Artist("Radiohead", "Alt Rock", 38, 4.28);
+        Artist artist2 = new Artist("Fiona Apple", "Alt Pop", 27, 4.01);
+        Artist artist3 = new Artist("TXT", "KPop", 4, 3.47);
         
-        String artist2 = "Fiona Apple";
-        String genre2 = "Alt Pop";
-        int yrs2 = 27;
-        double rate2 = 4.01;
-        System.out.println("Artist 2\n Name: " + artist2 + "\n Genre: " + genre2 + "\n Years active: " + yrs2 + "\n Highest Album Rating: " + rate2 + "\n~~~~~~~~~~~~~~~~~~~~");
+        // Song class
+        Song song1 = new Song("40 Days", "Slowdive", 3.22, 38);
+        Song song2 = new Song("Aftershow", "Megumi Acorda", 5.03, 79);
         
-        String artist3 = "TXT";
-        String genre3 = "KPop";
-        int yrs3 = 4;
-        double rate3 = 3.47;
-        System.out.println("Artist 3\n Name: " + artist3 + "\n Genre: " + genre3 + "\n Years active: " + yrs3 + "\n Highest Album Rating: " + rate3 + "\n~~~~~~~~~~~~~~~~~~~~");
+        // Start
+        Singer mySinger = new Singer("Megumi Acorda", 0, 0.00, song1);
+        System.out.println("Artist: " + mySinger.name + "\nNumber of Performances: " + mySinger.noOfPerformances + "\nEarnings: " + mySinger.earnings + "\nFavorite Song: " + mySinger.favoriteSong);
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
         
-        // Comparison
-        int active = yrs1 + yrs2 + yrs3;
-        System.out.println("Total years active: " + active);
-      
-        boolean years = yrs2>=yrs1;
-        System.out.println(artist2 + " has been active for more years than " + artist1 + ": " + years);
-      
-        boolean genre = genre2.equals(genre3);
-        System.out.println(artist3 + " and " + artist2 + " have the same genre: " + genre);
+        // Megumi Acorda performs
+        mySinger.performForAudience(12);
+        System.out.println(mySinger.name + " performed for an audience of 12. The concert was opened by " + artist3.name + ".");
+        mySinger.changeFavSong(song2);
         
-        boolean rate = rate1>=rate3;
-        System.out.println(artist1 + " has a higher album rating than " + artist3 + ": " + rate);
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~\nArtist: " + mySinger.name + "\nNumber of Performances: " + mySinger.noOfPerformances + "\nEarnings: " + mySinger.earnings + "\nFavorite Song: " + mySinger.favoriteSong);
         
-        System.out.println("~~~~~~~~~~~~~~~~~~~~\n*Ratings via rateyourmusic.com");
+        // The other classes interact
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~\n~~~~~~~~~~~~~~~~~~~~~~~~~\n" + song1.title + " No. of Streams: " + song1.streams + "\nLength: " + song1.minutes);
+        song1.newStream(3);
+        song1.newRemix(1.03);
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~\n" + artist2.name + " listened to " + song1.title + " 3 times.\n" + artist1.name + " released a remix of " + song1.title + " that is 1.03 minutes longer.");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~\n" + song1.title + " No. of Streams: " + song1.streams + "\nLength: " + song1.minutes);
+        
     }
     
 }
