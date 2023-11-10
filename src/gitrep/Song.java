cd53b670456f1e5df022d2bdc3fe9463e6737ed3
bamtowi <91894409+bamtowi@users.noreply.github.com>
2023-11-10 11:09:06 +0800

    Update Song.java

--- src/gitrep/Song.java
+++ src/gitrep/Song.java
@@ -1,23 +1,55 @@
 package gitrep;
 
 public class Song {
-    String title;
-    String artist;
-    double minutes;
-    int streams;
+    private String title;
+    private String artist;
+    private double minutes;
+    private int streams;
     
     public Song(String t, String a, double m, int s){
-        title = t;
-        artist = a;
-        minutes = m;
-        streams = s;
+        this.title = t;
+        this.artist = a;
+        this.minutes = m;
+        this.streams = s;
     }
     
     public void newStream(int scrobbles){
-        streams += scrobbles;
+        setStreams(getStreams() + scrobbles);
     }
     
     public void newRemix(double time){
-        minutes += time;
+        setMinutes(getMinutes() + time);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public double getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(double minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getStreams() {
+        return streams;
+    }
+
+    public void setStreams(int streams) {
+        this.streams = streams;
     }
 }
