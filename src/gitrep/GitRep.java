8b344ed964252c5f0cff3a5cddec204d910a81f1
bamtowi <91894409+bamtowi@users.noreply.github.com>
2023-11-10 11:20:27 +0800

    Update GitRep.java

--- src/gitrep/GitRep.java
+++ src/gitrep/GitRep.java
@@ -1,41 +1,85 @@
 package gitrep;
 
+import java.util.Scanner;
+
 public class GitRep {
 
     public static void main(String[] args) {
-        // Objects
-        String artist1 = "Radiohead";
-        String genre1 = "Alt Rock";
-        int yrs1 = 38;
-        double rate1 = 4.28;
-        System.out.println("Artist 1\n Name: " + artist1 + "\n Genre: " + genre1 + "\n Years active: " + yrs1 + "\n Highest Album Rating: " + rate1 + "\n~~~~~~~~~~~~~~~~~~~~");
+        Scanner sc;
+        sc = new Scanner(System.in);
         
-        String artist2 = "Fiona Apple";
-        String genre2 = "Alt Pop";
-        int yrs2 = 27;
-        double rate2 = 4.01;
-        System.out.println("Artist 2\n Name: " + artist2 + "\n Genre: " + genre2 + "\n Years active: " + yrs2 + "\n Highest Album Rating: " + rate2 + "\n~~~~~~~~~~~~~~~~~~~~");
+	Move rock = new Move("Rock");
+	Move paper = new Move("Paper");
+	Move scissors = new Move("Scissors");
+		
+	rock.setStrongAgainst(scissors);
+	paper.setStrongAgainst(rock);
+	scissors.setStrongAgainst(paper);
+		
+        int opt;
+        int botPts = 0;
+        int urPts = 0;
+	int roundsToWin = 2;
         
-        String artist3 = "TXT";
-        String genre3 = "KPop";
-        int yrs3 = 4;
-        double rate3 = 3.47;
-        System.out.println("Artist 3\n Name: " + artist3 + "\n Genre: " + genre3 + "\n Years active: " + yrs3 + "\n Highest Album Rating: " + rate3 + "\n~~~~~~~~~~~~~~~~~~~~");
+        do {
+            System.out.println("\nWelcome to Rock, Paper, Scissors. Please choose an option:\n1. Start Game\n2. Change number of rounds (default: 2)\n3. Exit application");
+            opt = sc.nextInt();
         
-        // Comparison
-        int active = yrs1 + yrs2 + yrs3;
-        System.out.println("Total years active: " + active);
-      
-        boolean years = yrs2>=yrs1;
-        System.out.println(artist2 + " has been active for more years than " + artist1 + ": " + years);
-      
-        boolean genre = genre2.equals(genre3);
-        System.out.println(artist3 + " and " + artist2 + " have the same genre: " + genre);
+            switch (opt) {
+                case 1 -> {
+                    System.out.println("\nThis match will be first to " + roundsToWin + " wins.");
+                    while (botPts < roundsToWin && urPts < roundsToWin){
+                        int rdNum = (int)(Math.random() * 3);
+                        Move botMove;
+                        botMove = switch (rdNum) {
+                            case 0 -> rock;
+                            case 1 -> paper;
+                            default -> scissors;
+                        };
+                    
+                        System.out.println("\nThe computer has selected its move. Select your move:\n1. Rock\n2. Paper\n3. Scissors");
+                    
+                        int urNum = sc.nextInt();
+                        Move urMove;
+                        urMove = switch (urNum) {
+                            case 1 -> rock;
+                            case 2 -> paper;
+                            default -> scissors;
+                        };
+                    
+                        int result = Move.compareMoves(botMove, urMove);
+                        String winner;
+                        switch (result) {
+                            case 0 -> {
+                                winner = "Computer";
+                                botPts++;
+                            }
+                            case 1 -> {
+                                winner = "Player";
+                                urPts++;
+                            }
+                            default -> winner = "No one";
+                        }
+                        
+                        System.out.println("\nPlayer chose " + urMove.getName() + ". Computer chose " + botMove.getName() + ". " + winner + " wins Round!");
+                        System.out.println("Player: " + urPts + " - Computer: " + botPts);
+                        
+                        if(botPts == roundsToWin || urPts == roundsToWin){
+                            System.out.println("\n" + winner + " wins the game!");
+                            opt = 3;
+                        }
+                    }
+                }
+                case 2 -> {
+                    System.out.print("\nHow many wins are needed to win a match?: ");
+                    roundsToWin = sc.nextInt();
+                    System.out.println("\nNew setting has been saved! Wins needed: " + roundsToWin);
+                }
+                default -> System.out.print("Thank you for playing!");
+            }
+        } while (opt==1 || opt==2);
         
-        boolean rate = rate1>=rate3;
-        System.out.println(artist1 + " has a higher album rating than " + artist3 + ": " + rate);
-        
-        System.out.println("~~~~~~~~~~~~~~~~~~~~\n*Ratings via rateyourmusic.com");
+        sc.close();
     }
     
 }
