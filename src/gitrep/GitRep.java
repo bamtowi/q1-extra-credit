53568413e2f18fbd5b4c05e2f30456132bdcc95c
bamtowi <91894409+bamtowi@users.noreply.github.com>
2023-11-10 11:29:48 +0800

    Update GitRep.java

--- src/gitrep/GitRep.java
+++ src/gitrep/GitRep.java
@@ -1,85 +1,56 @@
 package gitrep;
 
-import java.util.Scanner;
-
 public class GitRep {
 
     public static void main(String[] args) {
-        Scanner sc;
-        sc = new Scanner(System.in);
-        
-	Move rock = new Move("Rock");
-	Move paper = new Move("Paper");
-	Move scissors = new Move("Scissors");
-		
-	rock.setStrongAgainst(scissors);
-	paper.setStrongAgainst(rock);
-	scissors.setStrongAgainst(paper);
-		
-        int opt;
-        int botPts = 0;
-        int urPts = 0;
-	int roundsToWin = 2;
-        
-        do {
-            System.out.println("\nWelcome to Rock, Paper, Scissors. Please choose an option:\n1. Start Game\n2. Change number of rounds (default: 2)\n3. Exit application");
-            opt = sc.nextInt();
-        
-            switch (opt) {
-                case 1 -> {
-                    System.out.println("\nThis match will be first to " + roundsToWin + " wins.");
-                    while (botPts < roundsToWin && urPts < roundsToWin){
-                        int rdNum = (int)(Math.random() * 3);
-                        Move botMove;
-                        botMove = switch (rdNum) {
-                            case 0 -> rock;
-                            case 1 -> paper;
-                            default -> scissors;
-                        };
-                    
-                        System.out.println("\nThe computer has selected its move. Select your move:\n1. Rock\n2. Paper\n3. Scissors");
-                    
-                        int urNum = sc.nextInt();
-                        Move urMove;
-                        urMove = switch (urNum) {
-                            case 1 -> rock;
-                            case 2 -> paper;
-                            default -> scissors;
-                        };
-                    
-                        int result = Move.compareMoves(botMove, urMove);
-                        String winner;
-                        switch (result) {
-                            case 0 -> {
-                                winner = "Computer";
-                                botPts++;
-                            }
-                            case 1 -> {
-                                winner = "Player";
-                                urPts++;
-                            }
-                            default -> winner = "No one";
-                        }
-                        
-                        System.out.println("\nPlayer chose " + urMove.getName() + ". Computer chose " + botMove.getName() + ". " + winner + " wins Round!");
-                        System.out.println("Player: " + urPts + " - Computer: " + botPts);
-                        
-                        if(botPts == roundsToWin || urPts == roundsToWin){
-                            System.out.println("\n" + winner + " wins the game!");
-                            opt = 3;
-                        }
-                    }
-                }
-                case 2 -> {
-                    System.out.print("\nHow many wins are needed to win a match?: ");
-                    roundsToWin = sc.nextInt();
-                    System.out.println("\nNew setting has been saved! Wins needed: " + roundsToWin);
-                }
-                default -> System.out.print("Thank you for playing!");
-            }
-        } while (opt==1 || opt==2);
-        
-        sc.close();
+        // Create items
+    Item i1 = new Item("Burger", "food", 50);
+    Item i2 = new Item("Fries", "food", 30);
+    Item i3 = new Item("Fried Chicken", "food", 70);
+
+    Item i4 = new Item("Shampoo", "toiletries", 120);
+    Item i5 = new Item("Soap", "toiletries", 65);
+    Item i6 = new Item("Toothpaste", "toiletries", 95);
+
+    Item i7 = new Item("T-shirt", "clothes", 200);
+    Item i8 = new Item("Jeans", "clothes", 1200);
+    Item i9 = new Item("Shoes", "clothes", 2300);
+
+    // Create Stores
+    Store mcdo = new Store("McDo");
+    Store watsons = new Store("Watsons");
+    Store mall = new Store("Supermall");
+
+    mcdo.addItem(i1);
+    mcdo.addItem(i2);
+    mcdo.addItem(i3);
+
+    watsons.addItem(i4);
+    watsons.addItem(i5);
+    watsons.addItem(i6);
+
+    mall.addItem(i1);
+    mall.addItem(i2);
+    mall.addItem(i4);
+    mall.addItem(i5);
+    mall.addItem(i6);
+    mall.addItem(i7);
+    mall.addItem(i8);
+    mall.addItem(i9);
+
+    mall.filterType("food");
+    mall.filterCheap(500);
+    mall.filterExpensive(1000);
+
+    mcdo.sellItem(0);
+    mcdo.sellItem(50);
+    watsons.sellItem("Soap");
+    watsons.sellItem("Mouthwash");
+    mall.sellItem(i7);
+    mall.sellItem(i3);
+
+    // Check final earnings
+    Store.printStats();
     }
     
 }
